1bf46a728ac1c8c8146d7ff7e03ad2a3f6c1a8cb
Jiven Timsina <86583743+Jiventim@users.noreply.github.com>
2024-10-15 21:59:03 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 
 # symlinks for Oh My Posh theme
-ln -sf ~/dotfiles/oh-my-posh/theme.json ~/.poshthemes/your-theme-name.json
+ln -sf ~/dotfiles/oh-my-posh/powerlevel10k_lean.omp.json ~/.poshthemes/powerlevel10k_lean.omp.json
 
 # Source .bashrc
 if [ -f ~/.bashrc ]; then
