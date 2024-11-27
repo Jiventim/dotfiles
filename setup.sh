aab093302a46eda2345d3ee13c9672074f4ad31b
timsinajiven00@gmail.com <timsinajiven00@gmail.com>
2024-11-27 19:57:44 +0545

    update script

--- setup.sh
+++ setup.sh
@@ -10,6 +10,3 @@ ln -sf ~/.dotfiles/nvim ~/.config/nvim
 
 #zsh
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh/custom-theme ~/.oh-my-zsh/themes/custom.zsh-theme
-ln -sf ~/.dotfiles/zsh/aliases ~/.oh-my-zsh/custom/aliases.zsh
-chmod +x ~/.oh-my-zsh/custom/aliases.zsh
