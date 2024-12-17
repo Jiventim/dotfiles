d9f611bf59fe8632f41a14f59615b2bafa607d85
JivenTimsina <timsinajiven00@gmail.com>
2024-12-17 19:20:30 +0545

    update nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- In your Neovim configuration file
+
+vim.opt.relativenumber = true -- relativenumber
+
+-- Tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
